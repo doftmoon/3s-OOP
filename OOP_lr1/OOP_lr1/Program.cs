0c0a00e60039192ee20f23a6e892cedd1efff7d8
DoftMoon <DoftMoon@users.noreply.github.com>
2023-10-21 00:36:48 +0300

    Update Program.cs

--- OOP_lr1/OOP_lr1/Program.cs
+++ OOP_lr1/OOP_lr1/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Enter you name");
+string str = Console.ReadLine();
+Console.WriteLine("Hello " + str + "!!!");
+Console.WriteLine("Enter one sim from kb");
+int kod = Console.Read();
+char sim = (char)kod;
+Console.WriteLine("Cod of sim  " + sim + " = " + kod);
