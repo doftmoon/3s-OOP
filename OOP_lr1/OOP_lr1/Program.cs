176433f16bccc7f9f768f9bd5c048bf487bf76d1
doftmoon <DoftMoon@users.noreply.github.com>
2023-11-17 19:42:43 +0300

    update

--- OOP_lr1/OOP_lr1/Program.cs
+++ OOP_lr1/OOP_lr1/Program.cs
@@ -1,7 +1,2 @@
-﻿Console.WriteLine("Enter you name");
-string str = Console.ReadLine();
-Console.WriteLine("Hello " + str + "!!!");
-Console.WriteLine("Enter one sim from kb");
-int kod = Console.Read();
-char sim = (char)kod;
-Console.WriteLine("Cod of sim  " + sim + " = " + kod);
+﻿string str = "This is a text string";
+Console.WriteLine(str.Substring(4, str.Length - 4));
